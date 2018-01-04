908cc8de9b1887d4c424f694bff55d7f34e8a194
Tomasz Opalach <tomasz.opalach@gmail.com>
2018-01-04 12:43:21 +0100

    add empty lines

--- build.ps1
+++ build.ps1
@@ -174,3 +174,9 @@ write-host "Done creating packages."
 if ($buildType -eq 'stable') {
     BumpVersion $PROJECT_DIR $versionObj.VersionPrefix $versionObj.BuildType $DryRunVersionBump
 }
+
+
+
+
+
+
