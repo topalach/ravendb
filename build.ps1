700a0debb0f69dd06212e8497b336c0d38aa5ae5
Tomasz Opalach <tomasz.opalach@gmail.com>
2018-01-05 14:19:59 +0100

    another PR

--- build.ps1
+++ build.ps1
@@ -175,5 +175,5 @@ if ($buildType -eq 'stable') {
     BumpVersion $PROJECT_DIR $versionObj.VersionPrefix $versionObj.BuildType $DryRunVersionBump
 }
 
-# this is PR #10 - approach no. 21
+# this is PR #10 - approach no. 22
 
